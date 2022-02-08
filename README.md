e549802d4f59f6f334d400e8a9d31e52a4ea11e1
hanandadras <hanan.aabid@gmail.com>
2022-02-08 13:16:31 -0800

    updated timer and readMe file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # quizcoder
+quizcoder is an application that uses html, css and javascript.
+The user clicks on the start button to start a multiple choice quiz. When he clicks of the wrong  answer, he's penalized by substractiong 10 sec from the timer. When the answe is correct he moves to the next and is presented with a prompt to enter his initials and is presented with a final score next to his intials.
+The app goes back to the start of the quiz.
---
+++ assets/script.js
@@ -0,0 +1,148 @@
+
+const welcomeEl = document.getElementById('welcomeEl');
+const startBtn = document.getElementById('startBtn');
+
+const quizEl = document.getElementById('quizEl');
+const questionH2El = document.getElementById('questionH2El');
+const answerBtnEl1 = document.getElementById('answerBtnEl1');
+const answerBtnEl2 = document.getElementById('answerBtnEl2');
+const answerBtnEl3 = document.getElementById('answerBtnEl3');
+const answerBtnEl4 = document.getElementById('answerBtnEl4');
+const scoreEl = document.getElementById('scoreEl');
+var time = questions.length *15;
+var currentQuestionsIndex =0;
+var timerId;
+
+
+
+// questions and answers array in a var
+const allQuestions = [
+      {
+        question:"Commonly used data types Do Not <br> Include:",
+        Answer1:"strings",
+        Answer2:"booleans",
+        Answer3:"alerts",
+        Answer4:"numbers",
+        Correct:"Answer3",
+      },
+      {
+        question:"The condition in an if / else statement is <br> enclosed with___________",
+        Answer1:"quotes",
+        Answer2:"curly brackets",
+        Answer3:"parenthesis",
+        Answer4:"square brackets",
+        Correct:"Answer2",
+      },
+      {
+        question:"Arrays in Javascript can be used to store<br>_______.",
+        Answer1:"numbers and strings",
+        Answer2:"other arrays",
+        Answer3:"booleans",
+        Answer4:"all of the above",
+        Correct:"Answer1",
+      },
+      {
+        question:"String values must be enclosed within__________when being assigned to variables.",
+        Answer1:"commas",
+        Answer2:"curly brackets",
+        Answer3:"quotes",
+        Answer4:"parenthesis",
+        Correct:"Answer3",
+      },
+      {
+      
+        question:"A very useful tool used during development <br> and debugging for printing content to the <br> debugger is:",
+        Answer1:"Javascript",
+        Answer2:"Terminal/Bash",
+        Answer3:"for loops",
+        Answer4:"console.log",
+        Correct:"Answer4",
+      },
+      ]
+
+let questionIndex = 0
+
+// when user clicks start
+function startQuiz() {
+    // - hide welcome message
+    welcomeEl.classList.add('hide')
+    // - display quiz container
+    quizEl.classList.remove('hide')
+
+ // - start timer
+   
+function startTimer() {
+    const date = new Date();
+    document.getElementById("Timer").innerHTML = date.toLocaleTimeString();
+    setTimeout(function() {startTime()}, 75000);
+  }
+
+
+// // show timer start
+//   timerEl.textContent = timer;
+
+function loadNextQuestion() {
+    const currentQuestion = allQuestions[questionIndex]
+    console.log(currentQuestion)
+    // - display question text
+    questionH2El.innerText = currentQuestion.question
+
+    // - display answer buttons
+    answerBtnEl1.innerText = currentQuestion.Answer1
+    answerBtnEl2.innerText = currentQuestion.Answer2
+    answerBtnEl3.innerText = currentQuestion.Answer3
+    answerBtnEl4.innerText = currentQuestion.Answer4
+};
+loadNextQuestion();
+
+
+// - check if answer is correct
+function checkAnswer(event) {
+    const buttonText = event.target.innerText
+    const correct = allQuestions[questionIndex].correct
+    if (buttonText !== correct) {
+        console.log(buttonText=false)
+        timerEl-=5
+
+    } else {
+        console.log(buttonText=true)
+        
+        // if correct
+        //    - display correct message
+
+        // Do we have another question?
+        if (questionIndex < allQuestions.length - 1) {
+            // if yes
+            //     - load question
+            questionIndex++
+            loadNextQuestion()
+        } else {
+            //  if no
+            console.log('load score section')
+            //      - stop timer
+            //      - display score
+            //      - display restart button
+        }
+    }
+}
+
+// clear current question
+
+quizEl.innerHtml ="";
+
+//add event listener to start quiz
+startBtn.addEventListener('click', startQuiz)
+
+//add event lsitener upon click to check answer
+document
+    .getElementById('answerButtons')
+    .addEventListener('click', checkAnswer)
+
+
+    //
+    function quizEnd() {
+        // stop timer
+        clearInterval(timerId);
+      
+
+
